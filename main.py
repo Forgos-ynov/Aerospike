550a808af62a43c05136cd5b7e9678e51a9c3939
Lord-forgos <johan.rousseau39@gmail.com>
2023-06-14 14:14:47 +0200

    last send

--- main.py
+++ main.py
@@ -2,14 +2,8 @@ from Aerospike import Aerospike
 
 aerospike = Aerospike(namespace="test", set_name="school")
 
-bins = {
-    'nom': 'Donald',
-    'prenom': 'John',
-    'age': 30
-}
 
-
-# aerospike.import_datas()
+aerospike.import_datas()
 
 # aerospike.create(key=3001, bins=bins)
 # aerospike.update_record(key=key, bins=bins)
@@ -21,6 +15,5 @@ records = aerospike.retrieve_all_records()
 
 
 aerospike.close_client()
-alpha = 1
 
 
